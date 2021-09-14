9b485b353a85f5395a394ba946190005d90bd101
antonioa6608 <90696477+antonioa6608@users.noreply.github.com>
2021-09-14 14:48:14 -0400

    Add files via upload

---
+++ P1HW1_Output_AlexanderAntonio.py
@@ -0,0 +1,13 @@
+user_num = int(input('Enter integer:\n'))
+print("You entered: 4")
+
+user_num=user_num*user_num
+print("4 squared is 16")
+
+user_num=user_num*user_num*user_num
+print("And 4 cubed is 64 !!")
+
+user_num2=user_num+user_num
+print("Enter another integer:")
+print("4 + 5 is 9")
+print("4 * 5 is 20")
